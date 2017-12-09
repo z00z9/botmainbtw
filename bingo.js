f3ac8282de0a388e26fc8abdbad97215a114c476
z00z9 <32077824+z00z9@users.noreply.github.com>
2017-12-08 23:03:59 -0500

    Update bingo.js
    change to just vale stream bingo

--- bingo.js
+++ bingo.js
@@ -4,18 +4,7 @@
         var args = event.getArgs();                                                      
                                 
         if (command.equalsIgnoreCase('bingo')) {
-            if (args && args.length) {
-                var board = args[0].toLowerCase();
-                if (board == "overwatch") {
-                    $.say("Overwatch Bingo! Get your personal card here to play along during stream: http://bingobaker.com/play/1427574");
-                } else if (board == "vale") {
                     $.say("Bingo personalized for Vale stream! Get your personal card here to play along during stream: http://bingobaker.com/play/1427598");
-                } else {
-                    $.say("Custom Bingo boards for Overwatch and Vale stream! To get the link type !bingo overwatch or !bingo vale");
-                }
-            } else {
-                $.say("Custom Bingo boards for Overwatch and Vale stream! To get the link type !bingo overwatch or !bingo vale");
-            }
             return;
         }
     });
