aa804e7a0efba6a8acf9a03aaf22d97ab826f602
z00z9 <32077824+z00z9@users.noreply.github.com>
2017-12-08 22:58:04 -0500

    Update bingo.js
    spelling

--- bingo.js
+++ bingo.js
@@ -9,7 +9,7 @@
                 if (board == "overwatch") {
                     $.say("Overwatch Bingo! Get your personal card here to play along during stream: http://bingobaker.com/play/1427574");
                 } else if (board == "vale") {
-                    $.say("Bingo personalized for Vale stream! Get your personal card here to play along during strim: http://bingobaker.com/play/1427598");
+                    $.say("Bingo personalized for Vale stream! Get your personal card here to play along during stream: http://bingobaker.com/play/1427598");
                 } else {
                     $.say("Custom Bingo boards for Overwatch and Vale stream! To get the link type !bingo overwatch or !bingo vale");
                 }
